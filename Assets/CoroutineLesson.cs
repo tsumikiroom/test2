19a7c23c294a1889d2a1b40f76ea4e97e4e6dda9
tsumikiroom <kawamura@takashikawamura.info>
2017-05-22 00:47:25 +0900

    tst
    test

---
+++ Assets/CoroutineLesson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineLesson : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("MoveSphere");
+	}
+	IEnumerator MoveSphere(){
+		for (int f = 10; f >= 0; f--) {
+			this.gameObject.transform.position = new Vector3 (f * 0.1f, 1, 1);
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
